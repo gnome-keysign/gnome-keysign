42f44fd8dd0c5912977b9a584c31014752eede51
andreimacavei <andrei_gabriel.macavei@cti.pub.ro>
2014-05-28 13:45:55 +0300

    Removed avahi scripts and created network module with avahi specialised classes

--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,9 @@
 from gi.repository import Gtk
 from Sections import KeySignSection, GetKeySection
 
+from network.AvahiPublisher import ServicePublisher
+from network.AvahiDiscover import ServiceDiscover
+
 class MainWindow(Gtk.Window):
 
     def __init__(self):
@@ -15,3 +18,12 @@ class MainWindow(Gtk.Window):
 
         # setup signals
         self.connect("delete-event", Gtk.main_quit)
+
+        # setup avahi publisher
+        service_publisher = ServicePublisher(name="GeysignService", port=9999)
+        service_publisher.publish()
+
+        # FIXME: gobject.MainLoop().run is blocking the main thread
+        # setup avahi discover
+        # service_discover = ServiceDiscover(stype='_http._tcp')
+        # service_discover.discover()
---
+++ network/AvahiPublisher.py
@@ -0,0 +1,56 @@
+import avahi
+import dbus
+import gobject
+from dbus.mainloop.glib import DBusGMainLoop
+
+__all__ = ["ServicePublisher"]
+
+class ServicePublisher:
+    """A simple class to publish a network service with zeroconf using
+    avahi.
+
+    """
+
+    def __init__(self, name, port, stype="_http._tcp",
+                 domain="", host="", text=""):
+        self.name = name
+        self.stype = stype
+        self.domain = domain
+        self.host = host
+        self.port = port
+        self.text = text
+
+    def publish(self):
+        bus = dbus.SystemBus()
+        server = dbus.Interface(bus.get_object(
+                                    avahi.DBUS_NAME,
+                                    avahi.DBUS_PATH_SERVER),
+                                avahi.DBUS_INTERFACE_SERVER)
+
+        group = dbus.Interface(
+                    bus.get_object(avahi.DBUS_NAME,
+                                   server.EntryGroupNew()),
+                    avahi.DBUS_INTERFACE_ENTRY_GROUP)
+
+        group.AddService(avahi.IF_UNSPEC, avahi.PROTO_UNSPEC,dbus.UInt32(0),
+                     self.name, self.stype, self.domain, self.host,
+                     dbus.UInt16(self.port), self.text)
+
+        group.Commit()
+        self.group = group
+
+    def unpublish(self):
+        self.group.Reset()
+
+
+def test():
+    service = ServicePublisher(name="TestService", port=3000)
+    print "Adding service '%s' of type '%s' ..." % ("TestService", "_http._tcp")
+
+    service.publish()
+    raw_input("Press any key to unpublish the service ")
+    service.unpublish()
+
+
+if __name__ == "__main__":
+    test()
