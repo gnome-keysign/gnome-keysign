7a9b2627cd7300a52a10a255159dc84bcba68652
andreimacavei <andrei_gabriel.macavei@cti.pub.ro>
2014-07-02 01:43:45 +0300

    MainWindow: Set up the AvahiBrowser
    The service discovery part should run as soon as possible, i.e. at the start of the application. For now the publishing of the service will be made manually from the script AvahiPublisher.py

--- MainWindow.py
+++ MainWindow.py
@@ -1,8 +1,9 @@
+from gi.repository import GLib
 from gi.repository import Gtk
+
+from network.AvahiBrowser import AvahiBrowser
 from Sections import KeySignSection, GetKeySection
 
-from network.AvahiPublisher import ServicePublisher
-from network.AvahiDiscover import ServiceDiscover
 
 class MainWindow(Gtk.Window):
 
@@ -20,16 +21,33 @@ class MainWindow(Gtk.Window):
         # setup signals
         self.connect("delete-event", Gtk.main_quit)
 
-        # publish service with avahi
-        service_publisher = ServicePublisher(name="GeysignService", port=9001)
-        service_publisher.publish()
+        self.avahi_browser = None
+        self.avahi_service_name = '_demo._tcp'
+        self.discovered_services = []
+        GLib.idle_add(self.setup_avahi_browser)
 
-        # browser for services with avahi
-        # service_discover = ServiceDiscover(stype='_http._tcp')
-        # service_discover.discover()
+    def setup_avahi_browser(self):
+        # FIXME: place a proper service string there
+        self.avahi_browser = AvahiBrowser(service=self.avahi_service_name)
+        self.avahi_browser.connect('new_service', self.on_new_service)
 
-if __name__ == "__main__":
-    window = MainWindow()
-    window.show_all()
+        return False
 
-    Gtk.main()
+
+    def on_new_service(self, browser, name, address, port):
+        if self.verify_service(name, address, port):
+            GLib.idle_add(self.add_discovered_service, name, address, port)
+
+
+    def verify_service(self, name, address, port):
+        '''A tiny function to return whether the service
+        is indeed something we are interested in'''
+        if name == self.avahi_service_name:
+            return True
+        else:
+            return False
+
+    def add_discovered_service(self, name, address, port):
+        self.discovered_services += (name, address, port)
+
+        return False
