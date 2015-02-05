6885508e3d67c8dc2ad8aaaf578909d4331d9e71
Tobias Mueller <muelli@cryptobitch.de>
2015-02-05 02:01:22 +0100

    Added a stand alone script for signing key data
    This could work, but doesn't right now.  But it seems as if signing keys
    is broken in general at the moment.

---
+++ keysign/gnome-keysign-sign-key.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#    Copyright 2015 Tobias Mueller <muelli@cryptobitch.de>
+#
+#    This file is part of GNOME Keysign.
+#
+#    GNOME Keysign is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    GNOME Keysign is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with GNOME Keysign.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import sys
+
+from GetKeySection import GetKeySection
+
+def main(args):
+    log = logging.getLogger()
+    log.debug('Running main with args: %s', args)
+    w = GetKeySection(None)
+    if not args:
+        raise ValueError("You need to give filesnames as args: %s" % args)
+    for fname in args:
+        data = open(fname, 'r').read()
+        w.sign_key_async(fingerprint=None, callback=None, data=data)
+    
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG,
+            format='%(name)s (%(levelname)s): %(message)s')
+    sys.exit(main(sys.argv[1:]))
