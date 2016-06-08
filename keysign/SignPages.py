c14fd66828ea18a3ae533be4404b007e31377b85
Tobias Mueller <muelli@cryptobitch.de>
2016-06-08 16:53:12 +0200

    SignPages: Append a MAC to the generated barcode

--- keysign/SignPages.py
+++ keysign/SignPages.py
@@ -53,6 +53,12 @@ def mac_verify(data, mac):
     return result
 
 
+
+def get_public_key_data(fpr):
+    keydata = Keyring().export_data(fpr)
+    return keydata
+
+
 def parse_sig_list(text):
     '''Parses GnuPG's signature list (i.e. list-sigs)
     
@@ -166,6 +172,14 @@ class KeyPresentPage(Gtk.HBox):
     def draw_qrcode(self):
         assert self.fpr
         data = 'OPENPGP4FPR:' + self.fpr
+        # FIXME: okay, this is really bad.
+        # We should really try to get the keydata from another
+        # channel. There is key-selected signal. Maybe we can use that.
+        keydata = get_public_key_data(self.fpr)
+        mac = mac_generate(keydata)
+        # FIXME: We probably want to urlencode the thing...
+        data += '?mac=%s' % mac
+        log.info("Shoving %r to the QRCode", data)
         self.qrcode.data = data
 
 
