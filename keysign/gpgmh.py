956517e9ee569a1e3517d93312819e0cb5563c2f
Tobias Mueller <muelli@cryptobitch.de>
2016-07-27 11:35:26 +0200

    gpgmh: Added Key and UID classes
    The main reason is to be able to replace the gpg providing library more
    easliy as we intend to switch to something gpgme based.
    
    Subclassing namedtuple gives us a nice repr and a little bit of sanity
    check when creating new objects.
    
    We're requiring the expiry for now as we're displaying the expiry of a
    "key" in one GUI element. We could have required the Key to only carry
    an expiry flag, but to make the interface more similar, we also require
    an expiry when creating a UID.  This may very well change.
    
    The classes may also get additional (synthetic) properties such as
    "expired". But we'll add them once we need them. Again, our goal is not
    the build a comprehensive and complete representation of a key but only
    of the elements that we need.

--- keysign/gpgmh.py
+++ keysign/gpgmh.py
@@ -16,8 +16,10 @@
 #    You should have received a copy of the GNU General Public License
 #    along with GNOME Keysign.  If not, see <http://www.gnu.org/licenses/>.
 
+from collections import namedtuple
 import logging
 from tempfile import NamedTemporaryFile
+import warnings
 
 
 from monkeysign.gpg import Keyring
@@ -349,6 +351,69 @@ def parse_uid(uid):
         email = tokens[1].replace('>','').strip()
     
     return (name, comment, email)
+
+
+class UID(namedtuple("UID", "expiry name comment email")):
+    "Represents an OpenPGP UID - at least to the extent we care about it"
+
+    @classmethod
+    def from_monkeysign(cls, uid):
+        "Creates a new UID from a monkeysign key"
+        uidstr = uid.uid
+        name, comment, email = parse_uid(uidstr)
+        expiry = uid.expire
+        return cls(expiry, name, comment, email)
+
+    def __format__(self, arg):
+        if self.comment:
+            s = "{name} ({comment}) <{email}>"
+        else:
+            s = "{name} <{email}>"
+        return s.format(**self._asdict())
+
+    def __str__(self):
+        return "{}".format(self)
+
+    @property
+    def uid(self):
+        "Legacy compatibility, use str() instead"
+        warnings.warn("Legacy uid, use '{}'.format() instead",
+                      DeprecationWarning)
+        return str(self)
+
+
+class Key(namedtuple("Key", "expiry fingerprint uidslist")):
+    "Represents an OpenPGP Key to extent we care about"
+
+    def __init__(self, expiry, fingerprint, uidslist,
+                       *args, **kwargs):
+        super(Key, self).__init__(expiry, fingerprint, uidslist)
+
+    def __format__(self, arg):
+        s  = "{fingerprint}\r\n"
+        s += '\r\n'.join(("  {}".format(uid) for uid in self.uidslist))
+# This is what original output looks like:
+# pub  [unknown] 3072R/1BF98D6D 1336669781 [expiry: 2017-05-09 19:09:41]
+#    Fingerprint = FF52 DA33 C025 B1E0 B910  92FC 1C34 19BF 1BF9 8D6D
+# uid 1      [unknown] Tobias Mueller <tobias.mueller2@mail.dcu.ie>
+# uid 2      [unknown] Tobias Mueller <4tmuelle@informatik.uni-hamburg.de>
+# sub   3072R/3B76E8B3 1336669781 [expiry: 2017-05-09 19:09:41]
+        return s.format(**self._asdict())
+
+    @property
+    def fpr(self):
+        "Legacy compatibility, use fingerprint instead"
+        warnings.warn("Legacy fpr, use the fingerprint property",
+                      DeprecationWarning)
+        return self.fingerprint
+
+    @classmethod
+    def from_monkeysign(cls, key):
+        "Creates a new Key from an existing monkeysign key"
+        uids = [UID.from_monkeysign(uid) for uid in  key.uidslist]
+        expiry = key.expiry
+        fingerprint = key.fpr
+        return cls(expiry, fingerprint, uids)
 
 
 ## Monkeypatching to get more debug output
