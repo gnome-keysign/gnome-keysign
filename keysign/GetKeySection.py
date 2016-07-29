gnome-keysign/gnome-keysign

commit 44f5740d0f7da5f2f523b8dcac4561cfaa0490db

Removed obsolete Key module

keysign/GetKeySection.py
@@ -28,7 +28,6 @@ from .compat import monkeysign_expired_keys, monkeysign_revoked_keys
 from .SignPages import ScanFingerprintPage, SignKeyPage, PostSignPage
 from .util import mac_verify
 from .util import sign_keydata_and_send as _sign_keydata_and_send
-from . import key
 
 from gi.repository import Gst, Gtk, GLib
 # Because of https://bugzilla.gnome.org/show_bug.cgi?id=698005

keysign/key.py
@@ -1,37 +1,0 @@
-#!/usr/bin/env python
-#    Copyright 2014 Tobias Mueller <muelli@cryptobitch.de>
-#    Copyright 2014 Andrei Macavei <andrei.macavei89@gmail.com>
-#
-#    This file is part of GNOME Keysign.
-#
-#    GNOME Keysign is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    GNOME Keysign is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with GNOME Keysign.  If not, see <http://www.gnu.org/licenses/>.
-
-
-class KeyError(Exception):
-    pass
-
-class Key:
-    @classmethod
-    def is_valid_fingerprint(cls, fingerprint):
-        if len(fingerprint) != 40:
-            return False
-        return True
-
-    def __init__(self, fingerprint):
-        if not self.is_valid_fingerprint(fingerprint):
-            raise KeyError("Fingerprint {} does not "
-                           "appear to be valid".format(fingerprint))
-
-        self.fingerprint = fingerprint
-

commit 188a54b21583561b03fc642b3133f81651fcbaa5

GetKeySection: Don't create a Key when scanning a barcode
This follows up on the removal of the key module in
44f5740d0f7da5f2f523b8dcac4561cfaa0490db.

All it did was to check whether the fingerprint looked okay. I assume
it'll explode later in the process when the fingerprint is not good.

Fixes: 44f5740d0f7da5f2f523b8dcac4561cfaa0490db

keysign/GetKeySection.py
@@ -172,22 +172,30 @@ class GetKeySection(Gtk.VBox):
 
     def on_barcode(self, sender, barcode, message, image):
         '''This is connected to the "barcode" signal.
+        
+        The function will advance the application if a reasonable
+        barcode has been provided.
+        
+        Sender is the emitter of the signal and should be the scanning
+        widget.
+        
+        Barcode is the actual barcode that got decoded.
+        
         The message argument is a GStreamer message that created
-        the barcode.'''
-
+        the barcode.
+        
+        When image is set, it should be the frame as pixbuf that
+        caused a barcode to be decoded.
+        '''
+        self.log.info("Barcode signal %r %r", barcode, message)
         parsed = self.parse_barcode(barcode)
-        fpr = parsed['fingerprint']
-
-        if fpr != None:
-            try:
-                pgpkey = key.Key(fpr)
-            except key.KeyError:
-                self.log.exception("Could not create key from %s", barcode)
-            else:
-                self.log.info("Barcode signal %s %s" %( pgpkey.fingerprint, message))
-                self.on_button_clicked(self.nextButton, pgpkey, message, image, parsed_barcode=parsed)
+        fingerprint = parsed['fingerprint']
+        if not fingerprint:
+            self.log.error("Expected fingerprint in %r to evaluate to True, "
+                           "but is %r", parsed, fingerprint)
         else:
-            self.log.error("data found in barcode does not match a OpenPGP fingerprint pattern: %s", barcode)
+            self.on_button_clicked(self.nextButton,
+                fingerprint, message, image, parsed_barcode=parsed)
 
 
     def download_key_http(self, address, port):
@@ -308,10 +316,9 @@ class GetKeySection(Gtk.VBox):
                 if args:
                     # If we call on_button_clicked() from on_barcode()
                     # then we get extra arguments
-                    pgpkey = args[0]
+                    fingerprint = args[0]
                     message = args[1]
                     image = args[2]
-                    fingerprint = pgpkey.fingerprint
                 else:
                     image = None
                     raw_text = self.scanPage.get_text_from_textview()