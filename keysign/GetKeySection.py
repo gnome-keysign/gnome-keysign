gnome-keysign/gnome-keysign

commit f0a39233f2c73d673fa611d6d7b6fa8a263b2160

GetKeySection: Timing the HTTP request out
If the target is not requesting, the UI freezes.  That's a bigger
problem, but we put a timeout on the request anyway.

keysign/GetKeySection.py
@@ -230,7 +230,10 @@ class GetKeySection(Gtk.VBox):
             params='',
             query='',
             fragment='')
-        return requests.get(url.geturl()).text
+        self.log.debug("Starting HTTP request")
+        data = requests.get(url.geturl(), timeout=5).text
+        self.log.debug("finished downloading %d bytes", len(data))
+        return data
 
     def try_download_keys(self, clients):
         for client in clients:

commit 9efd6cb12f2709be360c4fe3e2e7c2e7cd2d0c50

GetKeySection: Catching ReadTimeout when GETting HTTP data
The exception would have gone unnoticed which causes problems when the
first server times out, because then the loop doesn't continue.

Fixes: f0a39233f2c73d673fa611d6d7b6fa8a263b2160

keysign/GetKeySection.py
@@ -22,7 +22,7 @@ from urlparse import urlparse, parse_qs, ParseResult
 import os  # We're using os.environ once...
 
 import requests
-from requests.exceptions import ConnectionError
+from requests.exceptions import ConnectionError, ReadTimeout
 
 from .compat import gtkbutton
 from .SignPages import ScanFingerprintPage, SignKeyPage, PostSignPage
@@ -261,6 +261,10 @@ class GetKeySection(Gtk.VBox):
                 # FIXME : We probably have other errors to catch
                 self.log.exception("While downloading key from %s %i",
                                     address, port)
+            except ReadTimeout as e:
+                self.log.exception("Timeout while downloading key from %s %i",
+                                    address, port)
+        self.log.debug("Finished trying to download keys from %s", clients)
 
     def verify_downloaded_key(self, downloaded_data, fingerprint, mac=None):
         log.info("Verifying key %r with mac %r", fingerprint, mac)
@@ -296,6 +300,7 @@ class GetKeySection(Gtk.VBox):
         other_clients = self.sort_clients(other_clients, fingerprint)
 
         for keydata in self.try_download_keys(other_clients):
+            self.log.debug("Obtained keydata %r", keydata)
             if self.verify_downloaded_key(keydata, fingerprint, mac):
                 is_valid = True
             else: