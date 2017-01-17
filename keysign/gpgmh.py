0d77e96dc4fe002281109de1e1b607a4a487a920
Tobias Mueller <muelli@cryptobitch.de>
2017-01-17 17:17:44 +0100

    gpgmh: Use the TempSigningKeyring with a base_keyring
    Admitted, the API is sufficiently weird.  But we're getting rid of it,
    anyway.
    
    The reason for this change is to allow a test to specify a homedir, i.e.
    to not leak anything about the regular user's keyring.
    Before, the user's keys were visible, because the TempSigningKeyring
    copies the secret keys from a freh Keyring() is no base_keyring is
    provided. That fresh Keyring uses the user's regular keys.  We provide
    the homedir, because the TempSigningKeyring calls Keyring's __init__
    which sets the keyrings' homedir accordingly.

--- keysign/gpgmh.py
+++ keysign/gpgmh.py
@@ -265,7 +265,8 @@ def sign_keydata(keydata, error_cb=None, homedir=None):
     """
     log = logging.getLogger(__name__ + ':sign_keydata_encrypt')
 
-    tmpkeyring = TempSigningKeyring(homedir=homedir)
+    tmpkeyring = TempSigningKeyring(homedir=homedir,
+        base_keyring=Keyring(homedir=homedir))
     tmpkeyring.context.set_option('export-options', 'export-minimal')
     # Eventually, we want to let the user select their keys to sign with
     # For now, we just take whatever is there.
