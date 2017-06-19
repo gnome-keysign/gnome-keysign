b3ea8d94595ba3c4a19af6d48b03dcdb357ab2a9
RyuzakiKK <aasonykk@gmail.com>
2017-06-19 12:22:37 +0200

    wormhole: improve error handling

--- keysign/wormholereceive.py
+++ keysign/wormholereceive.py
@@ -35,7 +35,6 @@ class WormholeReceive:
         self.stop()
         self.w = wormhole.create(self.app_id, RENDEZVOUS_RELAY, reactor)
         # The following mod is required for Python 2 support
-        # TODO check if code is None
         self.w.set_code("%s" % str(self.code))
 
         # callback when we receive a message
@@ -43,15 +42,24 @@ class WormholeReceive:
 
     def _received(self, message):
         m = decode_message(message)
-        key_data = m["offer"]["message"]
-        log.info("Message received: {}".format(key_data))
-        if self.callback:
-            GLib.idle_add(self.callback, key_data.encode("utf-8"))
-
-        # send a reply with a message ack, this also ensures wormhole cli interoperability
-        reply = {"answer": {"message_ack": "ok"}}
-        reply_encoded = encode_message(reply)
-        return self.w.send_message(reply_encoded)
+        key_data = None
+        offer = m.get("offer", None)
+        if offer:
+            key_data = offer.get("message", None)
+        if key_data:
+            log.info("Message received: {}".format(key_data))
+            if self.callback:
+                GLib.idle_add(self.callback, key_data.encode("utf-8"))
+            # send a reply with a message ack, this also ensures wormhole cli interoperability
+            reply = {"answer": {"message_ack": "ok"}}
+            reply_encoded = encode_message(reply)
+            return self.w.send_message(reply_encoded)
+        else:
+            log.info("Unknown message received")
+            reply = {"error": "Unrecognized message: {}".format(m)}
+            reply_encoded = encode_message(reply)
+            return self.w.send_message(reply_encoded)
+            # TODO display the error to the user
 
     def stop(self, callback=None):
         if self.w:
