d01cf6e9b8a6fd48a18cfe7e00559fd6d63ca334
Dhiraj kadam <kadamdhiraj96@gmail.com>
2018-03-29 00:45:21 +0530

    Removed unwanted message

--- keysign/discover.py
+++ keysign/discover.py
@@ -39,7 +39,7 @@ class Discover:
             key_data = yield threads.deferToThread(self.discovery.find_key, self.userdata)
             log.debug("Received key successfully")
         except ValueError as e:
-            log.debug("Exception occured", exc_info=e)
+            log.debug("foo", exc_info=e)
             log.debug('Unable to get key_data, received malformed or altered key')
             key_data = None
             success = False
