c82515f4663e0c3c812c0ff21574c81c76f078e1
RyuzakiKK <aasonykk@gmail.com>
2018-05-22 13:54:12 +0200

    setup: add missing comma

--- setup.py
+++ setup.py
@@ -217,9 +217,9 @@ setup(
         'requests>=2.6',
         
         'qrcode',
-        'twisted'
+        'twisted',
         'future',
-        'magic-wormhole>=0.10.2'
+        'magic-wormhole>=0.10.2',
         #'monkeysign', # Apparently not in the cheeseshop
         # avahi # Also no entry in the cheeseshop
         # dbus # dbus-python is in the cheeseshop but not pip-able
